e2db755f328eb6212933bb096310fa608b4da6df
Jose Chavez <jchavez2@odin.cs.csubak.edu>
2019-08-28 08:21:26 -0700

    Lab 1 commit

--- lab1.cpp
+++ lab1.cpp
@@ -40,7 +40,7 @@ using namespace std;
 #include <X11/keysym.h>
 #include <GL/glx.h>
 
-const int MAX_PARTICLES = 2;
+const int MAX_PARTICLES = 2000;
 const float GRAVITY     = 0.1;
 
 //some structures
@@ -64,7 +64,7 @@ class Global {
 public:
 	int xres, yres;
 	Shape box;
-	Particle particle;
+	Particle particle[MAX_PARTICLES];
 	int n;
 	Global();
 } g;
@@ -217,11 +217,12 @@ void makeParticle(int x, int y)
 		return;
 	cout << "makeParticle() " << x << " " << y << endl;
 	//set position of particle
-	Particle *p = &g.particle;
+	Particle *p = &g.particle[g.n];
 	p->s.center.x = x;
 	p->s.center.y = y;
-	p->velocity.y = -4.0;
-	p->velocity.x =  1.0;
+	p->velocity.y = -0.2;
+	p->velocity.x =  ((double)rand() / (double) RAND_MAX) - 5;
+	p->velocity.x =  ((double)rand() / (double) RAND_MAX) - 5 + 0.2;
 	++g.n;
 }
 
@@ -246,6 +247,7 @@ void check_mouse(XEvent *e)
 			//Left button was pressed.
 			int y = g.yres - e->xbutton.y;
 			makeParticle(e->xbutton.x, y);
+			makeParticle(e->xbutton.x, y);
 			return;
 		}
 		if (e->xbutton.button==3) {
@@ -259,7 +261,10 @@ void check_mouse(XEvent *e)
 			savex = e->xbutton.x;
 			savey = e->xbutton.y;
 			//Code placed here will execute whenever the mouse moves.
-
+			
+			int y = g.yres - e->xbutton.y;
+			for (int i=0; i<10; i++)
+			    makeParticle(e->xbutton.x, y);
 
 		}
 	}
@@ -290,20 +295,28 @@ void movement()
 {
 	if (g.n <= 0)
 		return;
-	Particle *p = &g.particle;
+	for (int i = 0; i < g.n; i++) {
+
+	Particle *p = &g.particle[i];
 	p->s.center.x += p->velocity.x;
 	p->s.center.y += p->velocity.y;
+	p->velocity.y -= GRAVITY;
 
 	//check for collision with shapes...
-	//Shape *s;
-
-
+	Shape *s = &g.box;
+	if (p->s.center.y < s->center.y + s->height &&
+		p->s.center.x > s->center.x - s->width &&
+		p->s.center.x < s->center.x + s->width)
+		p->velocity.y = -(p->velocity.y * 0.5);
+	
 
 
 	//check for off-screen
 	if (p->s.center.y < 0.0) {
-		cout << "off screen" << endl;
-		g.n = 0;
+		//cout << "off screen" << endl;
+		g.particle[i] = g.particle[g.n-1]; 
+		--g.n;
+	};
 	}
 }
 
@@ -312,7 +325,7 @@ void render()
 	glClear(GL_COLOR_BUFFER_BIT);
 	//Draw shapes...
 	//draw the box
-	Shape *s;
+	Shape *s = &g.box;
 	glColor3ub(90,140,90);
 	s = &g.box;
 	glPushMatrix();
@@ -329,11 +342,12 @@ void render()
 	glPopMatrix();
 	//
 	//Draw particles here
-	if (g.n > 0) {
+	//if (g.n > 0) {
+	for (int i = 0; i < g.n; i++) {
 		//There is at least one particle to draw.
 		glPushMatrix();
 		glColor3ub(150,160,220);
-		Vec *c = &g.particle.s.center;
+		Vec *c = &g.particle[i].s.center;
 		w = h = 2;
 		glBegin(GL_QUADS);
 			glVertex2i(c->x-w, c->y-h);
